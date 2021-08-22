40411dd6ef2c6234f8bf558f1ff58b32ede33352
Gurashish <ggill_be19@thapar.edu>
2021-08-22 16:06:17 +0530

    my changes

--- easybuyandsell/frontend/src/Components/MyAds/MyAds.jsx
+++ easybuyandsell/frontend/src/Components/MyAds/MyAds.jsx
@@ -2,6 +2,7 @@ import React, {useContext,useEffect} from 'react'
 import './MyAds.css'
 import AdItem from '../AdItem/AdItem'
 import { Button, Grid } from '@material-ui/core'
+import {Link, NavLink} from 'react-router-dom';
 import axios from 'axios'
 import { AuthContext } from '../Firebase/currentUser'
 import { auth } from '../Firebase/firebase'
@@ -29,18 +30,26 @@ function MyAds() {
     },[auth.currentUser])
     return ( 
         <div className="myads_container">
-                <h1 className="title">Your Ads</h1>
+                {
+                    myProducts.length > 0 && <h1 className="myads_title">Your Ads</h1>
+                }
                 <div className="ads_grid">
                     <Grid container justify="center" spacing={4} >
                         {   
-                            myProducts.length > 0 ?
-                        ( myProducts.map((product, id)=>(
-                                <Grid item key={product.id} xs={12} md={6} lg={4}>
-                                    <AdItem product={product}/>
-                                </Grid>
-                            )))
+                            myProducts.length > 0 
+                            ?
+                                ( myProducts.map((product, id)=>(
+                                    <Grid item key={product.id} xs={12} md={6} lg={4}>
+                                        <AdItem product={product}/>
+                                    </Grid>
+                                )))
                             :
-                            <h1>No Ads</h1>
+                            <>
+                                <div className="no_ad">
+                                    <h1>You have not uploaded any Ad</h1>
+                                    <p>Start Adding <NavLink exact to="/sell">here</NavLink></p>
+                                </div>
+                            </>
                         }
                     </Grid>
                 </div>
